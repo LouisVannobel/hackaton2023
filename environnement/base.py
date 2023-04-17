15a3c4ce5203d0f091be063f2712c00798b2fd09
BayTeo <teo.bay@edu.devinci.fr>
2023-04-17 09:39:20 +0200

    mise à jour de l'entrepot

--- environnement/base.py
+++ environnement/base.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk  # Importer les modules pour travailler avec les images
 
 class Base:
     def __init__(self):
@@ -16,6 +17,10 @@ base = Base()
 # Simule l'ajout d'objets à la base par les robots
 base.ajouter_objet("Objet1")
 
+# Charger l'image
+image = Image.open("entrepot.jpg")
+image = image.resize((50, 50))  # Redimensionner l'image si nécessaire
+image = ImageTk.PhotoImage(image)  # Convertir l'image en format Tkinter
 
 # On crée une fenêtre, racine de notre interface
 Fenetre = Tk()
@@ -27,8 +32,9 @@ zone_stockage.pack()  # Affiche le Canvas
 # On ajoute des éléments pour représenter les objets stockés
 x, y = 50, 50
 for objet in base.stockage:
-    zone_stockage.create_rectangle(x, y, x + 50, y + 50, fill="blue")
+    zone_stockage.create_image(x, y, image=image, anchor="nw")  # Ajouter l'image au canvas
     x += 100
 
 # On démarre la boucle Tkinter qui s'interrompt quand on ferme la fenêtre
 Fenetre.mainloop()
+
