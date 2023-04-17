a3945700d94a1de64b0c035af52bbe7b42281264
LouisVannobel <94976473+LouisVannobel@users.noreply.github.com>
2023-04-17 10:07:37 +0200

    Update Premier_Robot.py

--- environnement/Premier_Robot.py
+++ environnement/Premier_Robot.py
@@ -7,7 +7,7 @@ map1 = tk.PhotoImage(file="fondcarte.png")
 # Créer un Canvas et ajouter l'image de fond
 zone_dessin = tk.Canvas(Fenetre, width=500, height=500, bd=8)
 zone_dessin.pack() #Affiche le Canvas 
-zone_dessin.create_image(0, 0, anchor=NW, image=map1)
+zone_dessin.create_image(0, 0, anchor=tk.NW, image=map1)
 zone_dessin.create_oval(150, 150, 350, 350, fill="orange", width=4) # On démarre la boucle Tkinter qui s'interrompt quand on ferme la fenêtre
 Fenetre.mainloop()
 
