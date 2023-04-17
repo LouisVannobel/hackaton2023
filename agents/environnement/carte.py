d66ad5be2ea413c5ad2dffff990098916fff4efc
BayTeo <teo.bay@edu.devinci.fr>
2023-04-17 10:39:54 +0200

    ajout de bornes aléatoires

--- agents/environnement/carte.py
+++ agents/environnement/carte.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 class BorneRecharge:
     def __init__(self, row, column):
@@ -18,8 +19,16 @@ class Tableau(tk.Frame):
                 self.cell[row, column] = tk.Entry(self, width=3)
                 self.cell[row, column].grid(row=row, column=column)
                 
-        for borne in self.bornes_recharge:
-            self.cell[borne.row, borne.column].config(bg="green")
+        self.place_random_bornes_recharge()
+        
+    def place_random_bornes_recharge(self):
+        indices = random.sample(range(self.rows * self.columns), len(self.bornes_recharge))
+        for i, borne in enumerate(self.bornes_recharge):
+            row = indices[i] // self.columns
+            column = indices[i] % self.columns
+            borne.row = row
+            borne.column = column
+            self.cell[row, column].config(bg="green")
             
     def mise_a_jour(self, row, column, text, color):
         self.cell[row, column].config(fg=color)
@@ -29,7 +38,7 @@ class Tableau(tk.Frame):
 
 if __name__ == '__main__':
     root = tk.Tk()
-    bornes_recharge = [BorneRecharge(5, 5), BorneRecharge(15, 10)]
+    bornes_recharge = [BorneRecharge(0, 0), BorneRecharge(0, 0)]  # Placeholder values for rows and columns
     tableau = Tableau(root, bornes_recharge=bornes_recharge)
     tableau.pack(side="top", fill="both", expand=True)
     root.mainloop()
